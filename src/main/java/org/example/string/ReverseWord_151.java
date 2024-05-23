19bcfd5e20bd0302b5ff9cf3fc0be132d5fbf215
Carlos D <dario20049@icloud.com>
2024-05-23 10:41:55 +0900

    ReverseWord_151

--- src/main/java/org/example/string/ReverseWord_151.java
+++ src/main/java/org/example/string/ReverseWord_151.java
@@ -1,4 +1,28 @@
 package org.example.string;
 
+import java.util.Stack;
+
 public class ReverseWord_151 {
+
+    public static String reverseWords(String s) {
+
+        StringBuilder result = new StringBuilder();
+
+        String[] arrayS = s.trim().split("\\s+");
+
+        for (int i = arrayS.length - 1; i >= 1; i--) {
+            result.append(arrayS[i]);
+            result.append(" ");
+        }
+        result.append(arrayS[0]);
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+        System.out.println(reverseWords(s));
+    }
+
+
 }
